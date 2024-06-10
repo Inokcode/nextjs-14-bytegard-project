5fb50ce62c2838fc15a72bea0f8179dd5d413440
Inokcode <regilearn@gmail.com>
2024-06-10 15:08:35 +0530

    post page added

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,18 @@
-export default function page() {
-  return <div>page</div>;
+import Link from "next/link";
+
+export default async function Page() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  const data = await response.json();
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+      <ul>
+        {data.posts.map((post: any) => (
+          <li key={post.id} className="mb-5">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
